731166352a5a8958299aa0d3fe1a708b03583913
Andrey Novikov <NovikovAA.it@relef.ru>
2020-04-25 23:36:31 +0300

    YTASK-3: create chat list

---
+++ src/components/ChatList/ChatList.mock.js
@@ -0,0 +1,81 @@
+export const date = new Date()
+
+export const chatList = [
+    {
+        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        author: 'The Shrek 2',
+        chatName: 'Group Chat',
+        lastMessage: 'tas odio. Ut sit amet...',
+        icon: 'react' 
+    },
+    {
+        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        author: 'The Shrek 1',
+        chatName: 'Group Chat',
+        lastMessage: 'tas odio. Ut sit amet...',
+        icon: 'react' 
+    },
+    {
+        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        author: 'The Shrek 1',
+        chatName: 'Group Chat',
+        lastMessage: 'tas odio. Ut sit amet...',
+        icon: 'react' 
+    },
+    {
+        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        author: 'The Shrek 2',
+        chatName: 'Group Chat',
+        lastMessage: 'tas odio. Ut sit amet...',
+        icon: 'react' 
+    },
+    {
+        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        author: 'The Shrek 2',
+        chatName: 'Group Chat',
+        lastMessage: 'tas odio. Ut sit amet...',
+        icon: 'react' 
+    },
+    {
+        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        author: 'The Shrek 1',
+        chatName: 'Group Chat',
+        lastMessage: 'tas odio. Ut sit amet...',
+        icon: 'react' 
+    },
+    {
+        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        author: 'Rick Dalton 1',
+        chatName: 'Group Chat',
+        lastMessage: 'tas odio. Ut sit amet...',
+        icon: 'react' 
+    },
+    {
+        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        author: 'Rick Dalton 1',
+        chatName: 'Group Chat',
+        lastMessage: 'tas odio. Ut sit amet...',
+        icon: 'react' 
+    },
+    {
+        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        author: 'Rick Dalton 2',
+        chatName: 'Group Chat',
+        lastMessage: 'tas odio. Ut sit amet...',
+        icon: 'react' 
+    },
+    {
+        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        author: 'Rick Dalton 1',
+        chatName: 'Group Chat',
+        lastMessage: 'tas odio. Ut sit amet...',
+        icon: 'react' 
+    },
+    {
+        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        author: 'Rick Dalton 1',
+        chatName: 'Group Chat',
+        lastMessage: 'tas odio. Ut sit amet...',
+        icon: 'react' 
+    }
+]
