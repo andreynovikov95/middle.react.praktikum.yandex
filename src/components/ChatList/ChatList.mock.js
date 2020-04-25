17f7ce9a2fb62b3630ded60b32a6b452d03e9110
Andrey Novikov <NovikovAA.it@relef.ru>
2020-04-26 00:40:53 +0300

    YTASK-4: add random date for chat bar

--- src/components/ChatList/ChatList.mock.js
+++ src/components/ChatList/ChatList.mock.js
@@ -1,78 +1,89 @@
-export const date = new Date()
+const date = new Date()
+const day = date.getDate()
+const month = date.getMonth()
+const year = date.getFullYear()
+
+const MAX_YEAR = 2019
+
+const getRandomDate = (maxRange, minRange = 1) => {
+    return minRange + Math.round(Math.random() * (maxRange - minRange))
+}
+
+const getDate = () => `${getRandomDate(day)}/${getRandomDate(month)}/${getRandomDate(year, MAX_YEAR)}`
 
 export const chatList = [
     {
-        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        date: getDate(),
         author: 'The Shrek 2',
         chatName: 'Group Chat',
         lastMessage: 'tas odio. Ut sit amet...',
         icon: 'react' 
     },
     {
-        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        date: getDate(),
         author: 'The Shrek 1',
         chatName: 'Group Chat',
         lastMessage: 'tas odio. Ut sit amet...',
         icon: 'react' 
     },
     {
-        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        date: getDate(),
         author: 'The Shrek 1',
         chatName: 'Group Chat',
         lastMessage: 'tas odio. Ut sit amet...',
         icon: 'react' 
     },
     {
-        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        date: getDate(),
         author: 'The Shrek 2',
         chatName: 'Group Chat',
         lastMessage: 'tas odio. Ut sit amet...',
         icon: 'react' 
     },
     {
-        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        date: getDate(),
         author: 'The Shrek 2',
         chatName: 'Group Chat',
         lastMessage: 'tas odio. Ut sit amet...',
         icon: 'react' 
     },
     {
-        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        date: getDate(),
         author: 'The Shrek 1',
         chatName: 'Group Chat',
         lastMessage: 'tas odio. Ut sit amet...',
         icon: 'react' 
     },
     {
-        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        date: getDate(),
         author: 'Rick Dalton 1',
         chatName: 'Group Chat',
         lastMessage: 'tas odio. Ut sit amet...',
         icon: 'react' 
     },
     {
-        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        date: getDate(),
         author: 'Rick Dalton 1',
         chatName: 'Group Chat',
         lastMessage: 'tas odio. Ut sit amet...',
         icon: 'react' 
     },
     {
-        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        date: getDate(),
         author: 'Rick Dalton 2',
         chatName: 'Group Chat',
         lastMessage: 'tas odio. Ut sit amet...',
         icon: 'react' 
     },
     {
-        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        date: getDate(),
         author: 'Rick Dalton 1',
         chatName: 'Group Chat',
         lastMessage: 'tas odio. Ut sit amet...',
         icon: 'react' 
     },
     {
-        date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        date: getDate(),
         author: 'Rick Dalton 1',
         chatName: 'Group Chat',
         lastMessage: 'tas odio. Ut sit amet...',
