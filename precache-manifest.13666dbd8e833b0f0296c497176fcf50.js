742ef0d28a4c02114cd4eaccc1fd8ba85bc051b9
Andrey Novikov <NovikovAA.it@relef.ru>
2020-05-03 04:04:47 +0300

    YTASK-12: add deploy

---
+++ precache-manifest.13666dbd8e833b0f0296c497176fcf50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62304d8ab984092f5e99e82812d709da",
+    "url": "/react-messenger/index.html"
+  },
+  {
+    "revision": "0f40581a619f29ba8a33",
+    "url": "/react-messenger/static/css/main.4e52c1bf.chunk.css"
+  },
+  {
+    "revision": "6ea425138764754d2608",
+    "url": "/react-messenger/static/js/2.564c9b8a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-messenger/static/js/2.564c9b8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f40581a619f29ba8a33",
+    "url": "/react-messenger/static/js/main.dd5573cc.chunk.js"
+  },
+  {
+    "revision": "d139a4c7205e9e7cbded",
+    "url": "/react-messenger/static/js/runtime-main.e659759f.js"
+  }
+]);
